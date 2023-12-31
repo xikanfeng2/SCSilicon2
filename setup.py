7d851d76d43ce73bc2aa881a54dc2338af3aecf9
Eric Feng <xikanfeng2@gmail.com>
2023-12-31 16:04:11 +0800

    update version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SCSilicon2",
-    version="1.0.0",
+    version="1.0.1",
     author="Xikang Feng",
     author_email="fxk@nwpu.edu.cn",
     description="SCSilicon2: a single-cell genomics simulator which cost-effectively simulates single-cell genomics reads with haplotype-specific copy number annotation.",
