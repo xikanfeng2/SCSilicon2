0f7f3b5c3860f93e47a4cd1fa8926e984cd456e1
Eric Feng <xikanfeng2@gmail.com>
2024-01-08 03:38:48 +0800

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,6 @@ default_role = "literal"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 extensions = [
-    "sphinx_copybutton",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.doctest",
@@ -45,42 +44,12 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.autosummary",
     "sphinx.ext.extlinks",
-    "matplotlib.sphinxext.plot_directive",
-    "sphinx_autodoc_typehints",  # needs to be after napoleon
     "git_ref",  # needs to be before scanpydoc.rtd_github_links
     "sphinx.ext.linkcode",
-    "sphinx_design",
-    "sphinxext.opengraph",
     *[p.stem for p in (HERE / "extensions").glob("*.py") if p.stem not in {"git_ref"}],
 ]
 
-# Generate the API documentation when building
-autosummary_generate = True
-autodoc_member_order = "bysource"
-# autodoc_default_flags = ['members']
-napoleon_google_docstring = False
-napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
-napoleon_use_rtype = True  # having a separate entry generally helps readability
-napoleon_use_param = True
-napoleon_custom_sections = [("Params", "Parameters")]
-todo_include_todos = False
-api_dir = HERE / "api"  # function_images
-myst_enable_extensions = [
-    "amsmath",
-    "colon_fence",
-    "deflist",
-    "dollarmath",
-    "html_image",
-    "html_admonition",
-]
-myst_url_schemes = ("http", "https", "mailto")
-nb_output_stderr = "remove"
-nb_execution_mode = "off"
-nb_merge_streams = True
 
-
-ogp_site_url = "https://scsilicon2.readthedocs.io/en/stable/"
 
 typehints_defaults = "braces"
 
@@ -100,37 +69,3 @@ html_css_files = ["css/override.css"]
 html_show_sphinx = False
 # html_logo = "_static/img/Scanpy_Logo_BrightFG.svg"
 html_title = "SCSilicon2"
-
-
-def setup(app: Sphinx):
-    """App setup hook."""
-    app.add_config_value(
-        "recommonmark_config",
-        {
-            "auto_toc_tree_section": "Contents",
-            "enable_auto_toc_tree": True,
-            "enable_math": True,
-            "enable_inline_math": False,
-            "enable_eval_rst": True,
-        },
-        True,
-    )
-
-
-# -- Options for other output formats ------------------------------------------
-
-htmlhelp_basename = f"{project}doc"
-doc_title = f"{project} Documentation"
-latex_documents = [(master_doc, f"{project}.tex", doc_title, author, "manual")]
-man_pages = [(master_doc, project, doc_title, [author], 1)]
-texinfo_documents = [
-    (
-        master_doc,
-        project,
-        doc_title,
-        author,
-        project,
-        "One line description of project.",
-        "Miscellaneous",
-    )
-]
